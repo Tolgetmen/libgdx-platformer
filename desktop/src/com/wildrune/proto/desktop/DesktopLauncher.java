bd2b3d2a36baf28bd1f49f6ac87ac72b69eaedf9
Mark van der Wal <wilds.mark@gmail.com>
2018-02-09 19:46:03 +0100

    initial commit

---
+++ desktop/src/com/wildrune/proto/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wildrune.proto.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wildrune.proto.Platformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Platformer(), config);
+	}
+}
